fc21d9af2219b0d1bf30ead2c85f6f4dc5d60da9
Cedric Schwyter <cedricschwyter@bluewin.ch>
2022-12-17 15:53:29 +0100

    feat:add statistical analysis

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,23 @@
-use std::process::exit;
+use std::error::Error;
+use std::fs::File;
+use std::io::Write;
 
 use rand::thread_rng;
 use rand::Rng;
 
 const SPINS: usize = 10000;
 
-fn main() {
-    let mut balance = 10000.0f32;
+fn simulate() -> f32 {
+    let mut balance = 100.0f32;
     let mut bet = 4.0f32;
+    let mut max_balance = 100.0f32;
 
     let mut prev_win = true;
     let mut rng = thread_rng();
-    for i in 0..SPINS {
+    for _i in 0..SPINS {
         let spin = rng.gen_range(0..37);
-        println!("bet: {}", bet);
         if spin == 0 || spin == 3 || spin == 6 || spin == 7 || spin == 10 {
             balance -= bet;
-            println!("lose");
             if prev_win {
                 bet = 32.0f32;
             } else {
@@ -26,13 +27,29 @@ fn main() {
         } else {
             prev_win = true;
             balance += bet / 8.0;
-            println!("win");
             bet = 4.0f32;
         }
         if balance <= 0.0f32 {
-            println!("no money: {}", i);
-            exit(0);
+            break;
         }
-        println!("{}", balance);
+        if balance > max_balance {
+            max_balance = balance;
+        }
     }
+    max_balance
+}
+
+const NUM_SIM: usize = 10000;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut data = vec![];
+    for i in 0..NUM_SIM {
+        let num = simulate() as usize;
+        data.push(format!("{},{}", i.to_string(), num.to_string()));
+    }
+
+    let mut file = File::create("./out.csv")?;
+    write!(&mut file, "{}", data.join("\n"))?;
+
+    Ok(())
 }
